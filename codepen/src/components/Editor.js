2489e24d931e5f0265477c54cf230bc0af85623f
Daniel Kuang <danielkuang7@github.com>
2020-10-29 08:07:58 -0400

    Imported codemirror

--- codepen/src/components/Editor.js
+++ codepen/src/components/Editor.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import 'codemirror/lib/codemirror.css'
 
 export default function Editor() {
     return (
         <div>
-            
+            Editor
         </div>
     )
 }
