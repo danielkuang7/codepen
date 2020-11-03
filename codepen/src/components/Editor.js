c6660180f1f35ebd1c23d1e8cc83ead77f39db9d
Daniel Kuang <danielkuang7@github.com>
2020-11-03 08:11:09 -0500

    Imported controlledEditor

--- codepen/src/components/Editor.js
+++ codepen/src/components/Editor.js
@@ -4,7 +4,7 @@ import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
-
+import { Controlled as ControlledEditor } from 'react-codemirror2'
 
 export default function Editor() {
     return (
