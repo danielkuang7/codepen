f787df37adba08e87ce28a57013cd89ca21c4b1b
Daniel Kuang <danielkuang7@github.com>
2021-01-15 08:11:40 -0500

    Set initialValue for if condition

--- codepen/src/hooks/useLocalStorage.js
+++ codepen/src/hooks/useLocalStorage.js
@@ -9,6 +9,12 @@ export default function useLocalStorage(key, initialValue) {
         const jsonValue = localStorage.getItem(prefixedKey)
 
         if (jsonValue != null) return JSON.parse(jsonValue)
+
+        if(typeof initialValue === 'function'){
+            return initialValue()
+        } else {
+            return initialValue
+        }
     })
 
     
